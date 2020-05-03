27a15309d66933b718b9f3e0a68f2059a9648002
Neil Menghani <nmenghani19@gmail.com>
2020-05-03 12:34:02 -0400

    Hide plot display for draw_bounding_boxes

--- boxes/bb_helper.py
+++ boxes/bb_helper.py
@@ -1,8 +1,4 @@
-import os
-import random
-
 import numpy as np
-import pandas as pd
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,12 +8,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-from data_helper import UnlabeledDataset, LabeledDataset
 from helper import collate_fn, convert_map_to_road_map
 
 from PIL import Image
 
-def draw_box(ax, corners):
+def draw_black_box(ax, corners):
     point_squence = torch.stack([corners[:, 0], corners[:, 1], corners[:, 3], corners[:, 2], corners[:, 0]])
     ax.plot(point_squence.T[0] * 10 + 400, -point_squence.T[1] * 10 + 400, color='black', linewidth=0.5)
     
@@ -31,18 +26,20 @@ def draw_bounding_boxes(coordinates):
 
     ### Draw Boxes ###
     for i, bb in enumerate(coordinates):
-        draw_box(ax, bb)
+        draw_black_box(ax, bb)
 
     ### Ensure DPI is Correct and Save Image ###
     DPI = 200
     matplotlib.rcParams['figure.dpi'] = DPI
     fig.set_size_inches(1007.0/float(DPI),1007.0/float(DPI)) 
     plt.savefig("temp.png", bbox_inches='tight', dpi=DPI)
+    plt.close(fig)
 
     ### Read Image and Convert to Tensor ###
     bb_image = Image.open("temp.png")
     bb_image = torchvision.transforms.functional.to_tensor(bb_image)
-    new_image = convert_map_to_road_map(bb_image)
-    print(new_image.shape)
-    fig, ax = plt.subplots()
-    ax.imshow(new_image, cmap='binary');
+    binary_bb_image = convert_map_to_road_map(bb_image)
+#     print(new_image.shape)
+#     fig, ax = plt.subplots()
+#     ax.imshow(new_image, cmap='binary');
+    return binary_bb_image
